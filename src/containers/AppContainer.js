a5c1a78399b4f15f664db48e53c340c8fcb9ca9e
Scott Coupar <scott.coupar@googlemail.com>
2021-05-12 21:22:56 +0100

    added spotify player

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -3,7 +3,13 @@ import React from 'react';
 const AppContainer = () => {
     return(
         <>
-        <p>app container</p>
+        <div className = "container">
+        <h1 className = "title">V A S A</h1>
+        <a href= "https://vasa.bandcamp.com" target ="blank">BUY HEROICS ON VINYL</a>
+        <div className = "spotify">
+        <iframe src="https://open.spotify.com/embed/album/4cA77X4YjmJUvKTOZezXYF" width="500" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        </div>
+        </div>
         </>
     )
 }
