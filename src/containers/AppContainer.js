5a51d69e5c90f1419f120deed34858f0efadb081
Scott Coupar <scott.coupar@googlemail.com>
2021-05-13 20:53:47 +0100

    made responsive

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -6,17 +6,19 @@ const AppContainer = () => {
     return(
         <>
         <div className = "container">
-        <h1 className = "title">V A S A</h1>
+        <h1 className = "title">VA</h1>
+        <div className = "video">
+        <iframe className = "player"width="400" height="200" src="https://www.youtube.com/embed/8KIcsZw2XNg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
         <a className = "link" href= "https://vasa.bandcamp.com" target ="blank">BUY HEROICS ON VINYL/CD</a>
         <a className = "link" href = "https://vasa.bandcamp.com/merch" target ="blank">BUY MERCH</a>
-        <div className = "spotify">
-        <iframe src="https://open.spotify.com/embed/album/4cA77X4YjmJUvKTOZezXYF" width="500" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <div className = "social-container">
+            <SocialIcon url = "https://open.spotify.com/artist/0qb2PA9d6mQD2RwY5MdfW5?si=IKmkI6FFT9KZZg5BLsVn1Q" target ="blank" network = "spotify" bgColor = "GoldenRod" className ="icon" style={{height:50, width:50}}/>
+            <SocialIcon url = "https://music.apple.com/us/artist/vasa/27755625" target ="blank" network = "itunes" bgColor = "GoldenRod" className ="icon" style={{height:50, width:50}}/>
+            <SocialIcon url = "https://instagram.com/vasaband" target ="blank" network = "instagram" bgColor = "GoldenRod" className ="icon" style={{height:50, width:50}}/>
+            <SocialIcon url ="https://www.facebook.com/vasaband" target ="blank" network = "facebook" bgColor = "GoldenRod"className ="icon" style={{height:50, width:50}}/>
+            <SocialIcon url = "https://twitter.com/vasaband" target ="blank" network = "twitter" bgColor = "GoldenRod" className ="icon" style={{height:50, width:50}}/>
         </div>
-            <div className = "social-container">
-                <SocialIcon url ="https://www.facebook.com/vasaband" target ="blank" network = "facebook" bgColor = "gold"className ="icon" style={{height:100, width:100}}/>
-                <SocialIcon url = "https://twitter.com/vasaband" target ="blank" network = "twitter" bgColor = "gold" className ="icon" style={{height:100, width:100}}/>
-                 <SocialIcon url = "https://instagram.com/vasaband" target ="blank" network = "instagram" bgColor = "gold" className ="icon" style={{height:100, width:100}}/>
-            </div>
         
         </div>
         </>
